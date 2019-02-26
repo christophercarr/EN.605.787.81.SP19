da79e74a873c71132fd4bcfa3374981393442946
Chris Carr <stephen.c.carr@jhuapl.edu>
2019-02-26 03:02:05 -0500

    Initial Coursera Req's

--- module4/SpeakGoodBye.js
+++ module4/SpeakGoodBye.js
@@ -3,12 +3,19 @@
 
 // STEP 6: Wrap the entire contents of SpeakGoodBye.js inside of an IIFE
 // See Lecture 52, part 2
+(function (window) { // Open the IIFE
 
+// Use the function constructor so that we have a better "class" to use.
+function SpeakGoodBye() {
+  // Empty, For now...
+}
 
 // STEP 7: Create an object, called 'byeSpeaker' to which you will attach
 // the "speak" method and which you will expose to the global context
 // See Lecture 52, part 1
-// var byeSpeaker =
+
+// Using an explicit "class" instead of the literal object creation.
+var byeSpeaker = new SpeakGoodBye();
 
 // DO NOT attach the speakWord variable to the 'byeSpeaker' object.
 var speakWord = "Good Bye";
@@ -16,10 +23,18 @@ var speakWord = "Good Bye";
 // STEP 8: Rewrite the 'speak' function such that it is attached to the
 // byeSpeaker object instead of being a standalone function.
 // See Lecture 52, part 2
-function speak(name) {
+
+// Use the prototype construct so that any/all created objects have the same
+// function declaration in the "class/function" level, instead of in the
+// individual instance memory space.
+SpeakGoodBye.prototype.speak = function(name) {
   console.log(speakWord + " " + name);
-}
+};
 
 // STEP 9: Expose the 'byeSpeaker' object to the global scope. Name it
 // 'byeSpeaker' on the global scope as well.
-// xxxx.xxxx = byeSpeaker;
+// See Lecture 52, part 2
+
+window.byeSpeaker = byeSpeaker;
+
+})(window); // Close & Execute The IIFE
--- module4/SpeakHello.js
+++ module4/SpeakHello.js
@@ -1,11 +1,21 @@
+// NOTE! The steps in this file are basically identical to the ones you
+// performed in the SpeakGoodBye.js file.
+
 // STEP 2: Wrap the entire contents of SpeakHello.js inside of an IIFE
 // See Lecture 52, part 2
+(function (window) { // Open the IIFE
 
+// Use the function constructor so that we have a better "class" to use.
+function SpeakHello() {
+  // Empty, For now...
+}
 
 // STEP 3: Create an object, called 'helloSpeaker' to which you will attach
 // the "speak" method and which you will expose to the global context
 // See Lecture 52, part 1
-// var helloSpeaker =
+
+// Using an explicit "class" instead of the literal object creation.
+var helloSpeaker = new SpeakHello();
 
 // DO NOT attach the speakWord variable to the 'helloSpeaker' object.
 var speakWord = "Hello";
@@ -13,12 +23,18 @@ var speakWord = "Hello";
 // STEP 4: Rewrite the 'speak' function such that it is attached to the
 // helloSpeaker object instead of being a standalone function.
 // See Lecture 52, part 2
-function speak(name) {
+
+// Use the prototype construct so that any/all created objects have the same
+// function declaration in the "class/function" level, instead of in the
+// individual instance memory space.
+SpeakHello.prototype.speak = function(name) {
   console.log(speakWord + " " + name);
-}
+};
 
 // STEP 5: Expose the 'helloSpeaker' object to the global scope. Name it
 // 'helloSpeaker' on the global scope as well.
 // See Lecture 52, part 2
 // (Note, Step 6 will be done in the SpeakGoodBye.js file.)
-// xxxx.xxxx = helloSpeaker;
+window.helloSpeaker = helloSpeaker;
+
+})(window); // Close & Execute The IIFE
