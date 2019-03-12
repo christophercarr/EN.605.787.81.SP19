093d4682df3503dede1b8c4329d9cfb08cb1f2f5
Chris Carr <stephen.c.carr@jhuapl.edu>
2019-03-12 03:05:41 -0400

    Baseline Requirements

---
+++ module6/js/app.js
@@ -0,0 +1,44 @@
+(function(global){
+
+   // Prevent variables from leaking into the parent scope.
+   'use strict';
+
+    // Define the overall module and the single controller for this app.
+   angular.module('LunchCheck', []).controller('LunchCheckController', LunchCheckController);
+
+   // Prevent minification issues by defining the dependency injection variables.
+   LunchCheckController.$inject = ['$scope'];
+
+   // Magic!
+   function LunchCheckController($scope) {
+     $scope.inputListValue = "";
+     $scope.outputMessage  = "";
+
+     $scope.submitList = function() {
+        var segmentCount = getSegments($scope.inputListValue).length;
+        var message      = getMessage(segmentCount);
+        $scope.outputMessage = message;
+     };
+   }
+
+   // Used to determine if the input value is empty, prior to attempting to split for values.
+   function isEmpty(string) {
+      return string === "";
+   }
+
+   // Used to split the input string into segments, using a comma "," as the separator.
+   function getSegments(string) {
+      return isEmpty(string) ? [ ] : string.split(",");
+   }
+
+   // Determine the appropriate message, based on segment counts.
+   function getMessage(count) {
+      if (count <= 0) {
+         return "Please enter data first.";
+      } else if (count <= 3) {
+         return "Enjoy!";
+      } else {
+         return "Too much!";
+      }
+   }
+})(window);
