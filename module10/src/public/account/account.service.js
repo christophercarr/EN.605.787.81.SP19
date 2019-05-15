b21c4fa6639dc53bd256860e4c0e3e8f6f71c2f1
Chris Carr <stephen.c.carr@jhuapl.edu>
2019-05-14 21:12:04 -0400

    Task #1 Complete

---
+++ module10/src/public/account/account.service.js
@@ -0,0 +1,33 @@
+(function () {
+    "use strict";
+
+    angular.module('public')
+        .service('AccountService', AccountService);
+
+    AccountService.$inject = [];
+
+    function AccountService() {
+        var service = this;
+
+        service.account = {
+            firstName:    null,
+            lastName:     null,
+            email:        null,
+            phone:        null,
+            favoriteDish: null
+        };
+
+        service.saveInfo = function (firstName, lastName, email, phone, favoriteDish) {
+            service.account.firstName    = firstName;
+            service.account.lastName     = lastName;
+            service.account.email        = email;
+            service.account.phone        = phone;
+            service.account.favoriteDish = favoriteDish;
+        };
+
+
+        service.getInfo = function () {
+            return service.account;
+        };
+    }
+})();
---
+++ module10/src/public/sign-up/sign-up.controller.js
@@ -0,0 +1,37 @@
+(function () {
+    "use strict";
+
+    angular.module('public')
+        .controller('SignUpController', SignUpController);
+
+    SignUpController.$inject = ['$http', 'ApiPath', 'AccountService'];
+
+    function SignUpController($http, ApiPath, AccountService) {
+        var controller = this;
+
+        controller.firstName    = null;
+        controller.lastName     = null;
+        controller.email        = null;
+        controller.phone        = null;
+        controller.favoriteDish = null;
+        controller.invalidDish  = false;
+        controller.infoSaved    = false;
+
+        controller.submit = function() {
+
+            var onSuccess = function (response) {
+                AccountService.saveInfo(controller.firstName, controller.lastName, controller.email, controller.phone, controller.favoriteDish);
+                controller.invalidDish = false;
+                controller.infoSaved   = true;
+            };
+
+            var onError = function (response) {
+                controller.invalidDish = true;
+                controller.infoSaved   = false;
+            };
+
+            $http.get(ApiPath + '/menu_items/' + controller.favoriteDish + '.json').then(onSuccess, onError);
+        };
+    }
+
+})();
